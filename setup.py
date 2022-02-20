ab126ac976166bd13e9de24d4a0d0ab1e37a293a
Dennis Kennetz <drkennetz@gmail.com>
2022-02-20 07:37:07 -0600

    created empty pypi to reserve name

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='jobq',
+    version='0.0.1',
+    description='local job queueing functionality',
+    long_description='',
+    url='https://github.com/drkennetz/jobq',
+    author='Dennis Kennetz',
+    author_email='drkennetz@gmail.com',
+    packages=[],
+    classifiers=['Development Status :: 1 - Planning'],
+)
